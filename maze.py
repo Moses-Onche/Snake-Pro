7352a34b266280d2f266bd62f094b152a4c2be4f
Moses-Onche <onchemoses38@gmail.com>
2023-05-26 23:46:45 +0100

    Create maze class

---
+++ maze.py
@@ -0,0 +1,5 @@
+class Maze:
+    """Defines all the elements of the stage Maze."""
+    def __init__(self, sp_main_game):
+        """Initialize the maze frame."""
+        self.screen = sp_main_game.screen 
