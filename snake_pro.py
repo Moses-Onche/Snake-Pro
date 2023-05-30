cb44f6437a7ceddacb4ed7f648d3724a5fedfe09
Moses-Onche <onchemoses38@gmail.com>
2023-05-30 20:02:47 +0100

    Implement boundary limit test

--- snake_pro.py
+++ snake_pro.py
@@ -4,6 +4,7 @@ import pygame
 import sys
 from maze import Maze
 from settings import Settings
+from snake import Snake
 
 
 class SnakePro:
@@ -16,16 +17,18 @@ class SnakePro:
         self.screen = pygame.display.set_mode((1200, 800))
         self.settings = Settings()
         self.border = Maze(self)
+        self.snake = Snake(self)
+
         pygame.display.set_caption("Snake Pro")
 
-    def game_run(self):
+    def game_loop(self):
         """Run function to execute all game loops."""
         while True:
+            pygame.time.delay(50)
+            
+            self.flip_screen()
             self.game_events()
-
-            self.screen.fill(self.settings.bg_color)
-            self.border.blitme()
-            pygame.display.flip()
+            self.snake.update_pos()
 
     def game_events(self):
         """Respond to mouse and keyboard events."""
@@ -34,14 +37,43 @@ class SnakePro:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 self.keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self.keyup_events(event)
 
     def keydown_events(self, event):
         """Respond when a key is pressed down."""
         if event.key == pygame.K_q:
             sys.exit()
+        if event.key == pygame.K_LEFT:
+            self.snake.m_left = True
+        if event.key == pygame.K_RIGHT:
+            self.snake.m_right = True
+        if event.key == pygame.K_UP:
+            self.snake.m_up = True
+        if event.key == pygame.K_DOWN:
+            self.snake.m_down = True
+
+    def keyup_events(self, event):
+        """Respond when a key is released."""
+        if event.key == pygame.K_LEFT:
+            self.snake.m_left = False
+        if event.key == pygame.K_RIGHT:
+            self.snake.m_right = False
+        if event.key == pygame.K_UP:
+            self.snake.m_up = False
+        if event.key == pygame.K_DOWN:
+            self.snake.m_down = False
+
+    def flip_screen(self):
+        """Flip screen as changes and events occur."""
+        self.screen.fill(self.settings.bg_color)
+        self.border.blitme()
+        self.snake.blitme()
+        
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
     """Main game instance."""
     sp_main = SnakePro()
-    sp_main.game_run()
+    sp_main.game_loop()
