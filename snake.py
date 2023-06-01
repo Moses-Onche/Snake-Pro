ae8409677c5eb728abc778760e9cde46d5e869d6
Moses-Onche <onchemoses38@gmail.com>
2023-06-01 22:13:52 +0100

    Build snake body

--- snake.py
+++ snake.py
@@ -8,42 +8,69 @@ class Snake:
         self.screen = game_obj.screen
         self.screen_rect = game_obj.screen.get_rect()
         
-        self.image = pygame.image.load('images/box.png')
-        self.rect = self.image.get_rect()
+        # Create head
+        self.h_image = pygame.image.load('images/head.png')
+        self.h_rect = self.h_image.get_rect()
+
+        # Create neck
+        self.n_image = pygame.image.load('images/neck.png')
+        self.n_rect = self.n_image.get_rect()
+
+        # Create body segment 1
+        self.b1_image = pygame.image.load('images/body1.png')
+        self.b1_rect = self.b1_image.get_rect()
+
+        # Create body segment 2
+        self.b2_image = pygame.image.load('images/body2.png')
+        self.b2_rect = self.b2_image.get_rect()
         
         # Start snake at the center of the screen.
-        self.rect.center = self.screen_rect.center
+        self.h_rect.center = self.screen_rect.center
+        self.n_rect.center = ((self.h_rect.center[0], self.h_rect.center[1] + 8))
+        self.b1_rect.center = ((self.n_rect.center[0], self.n_rect.center[1] + 8))
+        self.b2_rect.center = ((self.b1_rect.center[0], self.b1_rect.center[1] + 8))
 
         # Define border limits for snake movement.
         border_width = 372
         border_height = 225
-        self.left_border = self.rect.center[0] - border_width + 1
-        self.right_border = self.rect.center[0] + border_width
-        self.top_border = self.rect.center[1] - border_height - 15
-        self.bottom_border = self.rect.center[1] + border_height - 25
+        self.left_border = self.h_rect.center[0] - border_width + 1
+        self.right_border = self.h_rect.center[0] + border_width
+        self.top_border = self.h_rect.center[1] - border_height - 15
+        self.bottom_border = self.h_rect.center[1] + border_height - 25
 
         # Movement parameters
-        self.speed = 15
+        #self.move = 0
+        self.speed = 5
+        self.p_angle = 90
+        self.n_angle = self.p_angle - 90
 
-        """
         self.m = 10
         self.m_right = False
         self.m_left = False
         self.m_up = False
         self.m_down = False
-        """
 
     def update_pos(self):
-        """Update snake's position in response to movement."""
-        if self.m_right and self.rect.right < self.right_border:
-            self.rect.x += self.m
-        if self.m_left and self.rect.left > self.left_border:
-            self.rect.x -= self.m
-        if self.m_up and self.rect.top > self.top_border:
-            self.rect.y -= self.m
-        if self.m_down and self.rect.bottom < self.bottom_border:
-            self.rect.y += self.m
+        """Implement snake's continuous movement."""
+        if self.h_rect.left > self.left_border:
+            self.h_rect.x -= self.speed
+        if self.h_rect.right < self.right_border:
+            self.h_rect.x += self.speed
+        if self.h_rect.top > self.top_border:
+            self.h_rect.y -= self.speed
+        if self.h_rect.bottom < self.bottom_border:
+            self.h_rect.y += self.speed
+
+    """
+    def change_dir(self):
+        Change the direction of movement of the snake.
+        if self.m_right:
+    """
+
 
     def blitme(self):
         """Draw the maze."""
-        self.screen.blit(self.image, self.rect)
+        self.screen.blit(self.h_image, self.h_rect)
+        self.screen.blit(self.n_image, self.n_rect)
+        self.screen.blit(self.b1_image, self.b1_rect)
+        self.screen.blit(self.b2_image, self.b2_rect)
