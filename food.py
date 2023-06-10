72a606d87d7f42759c5341f162e81b904a96d102
Moses-Onche <onchemoses38@gmail.com>
2023-06-11 00:22:37 +0100

    Put randrange figures directly into food rect center argument

--- food.py
+++ food.py
@@ -7,15 +7,11 @@ class Food:
     def __init__(self, game_obj):
         """Initialize the food parameters."""
         self.screen =  game_obj.screen
-        self.screen_rect = self.screen.get_rect()
 
         self.f_image = pygame.image.load('images/food.png')
         self.f_rect = self.f_image.get_rect()
+        self.f_rect.center = (randrange(350, 900), randrange(250, 500))
 
-        x = randrange(350, 900)
-        y = randrange(250, 500)
-        self.f_rect.center = (x, y)
-
-    def blitme(self):
+    def blit(self):
         """Draw the food in the window."""
         self.screen.blit(self.f_image, self.f_rect)
